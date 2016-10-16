8beb75c9d289ef8dde877ceeb90c1742a12cccd5
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-16 16:06:41 -0400

    Add error handling to controller for exceeding capacity

--- test/controllers/reservation_controller_test.exs
+++ test/controllers/reservation_controller_test.exs
@@ -17,7 +17,7 @@ defmodule Hungry.ReservationControllerTest do
       assert conn.status == 200
     end
 
-    test "returns corrects result on validation error", %{conn: conn} do
+    test "returns correct result on validation error", %{conn: conn} do
       imp = fn -> {:error, "Invalid date."} end
       reservation_rendition = %{
         datetime: "2016-02-29T12:30:30.120+00:00Z",
@@ -30,6 +30,21 @@ defmodule Hungry.ReservationControllerTest do
       conn = post conn, reservation_path(conn, :create), reservation_rendition: reservation_rendition
 
       assert conn.status == 400
+    end
+
+    test "returns correct result on capacity exceeded", %{conn: conn} do
+      imp = fn -> {:error, :capacity_exceeded} end
+      reservation_rendition = %{
+        datetime: "2016-02-29T12:30:30.120+00:00Z",
+        name: "Mark Seemann",
+        email: "mark@ploeh.dk",
+        quantity: 4
+      }
+
+      conn = assign(conn, :imp, imp)
+      conn = post conn, reservation_path(conn, :create), reservation_rendition: reservation_rendition
+
+      assert conn.status == 422
     end
   end
 end
--- web/controllers/reservation_controller.ex
+++ web/controllers/reservation_controller.ex
@@ -5,6 +5,7 @@ defmodule Hungry.ReservationController do
     imp = conn.assigns[:imp]
     case imp.() do
       :ok -> send_resp(conn, 200, "OK")
+      {:error, :capacity_exceeded} -> send_resp(conn, 422, "Capacity Exceeded")
       {:error, _} -> send_resp(conn, 400, "Validation Error")
     end
   end
