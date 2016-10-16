452b9df1886878156e8e29162a0d5de43cf37faf
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-16 15:54:38 -0400

    Implement error path for checking capacity

--- test/models/capacity_test.exs
+++ test/models/capacity_test.exs
@@ -6,7 +6,7 @@ defmodule Hungry.CapacityTest do
   describe "check" do
     test "returns right result at no prior reservations" do
       capacity = 10
-      reserved_seats = 0
+      get_reserved_seats = fn(_) -> 0 end
       reservation = %{
         datetime: Timex.parse("2016-02-29T12:30:30.120+00:00Z", "{ISO:Extended}"),
         name: "Mark Seemann",
@@ -14,9 +14,25 @@ defmodule Hungry.CapacityTest do
         quantity: 4
       }
 
-      actual = Capacity.check(capacity, reserved_seats, reservation)
+      actual = Capacity.check(capacity, get_reserved_seats, reservation)
 
       expected = {:ok, reservation}
+      assert expected == actual
+    end
+
+    test "returns right result at too little remaining capacity" do
+      capacity = 10
+      get_reserved_seats = fn(_) -> 7 end
+      reservation = %{
+        datetime: Timex.parse("2016-02-29T12:30:30.120+00:00Z", "{ISO:Extended}"),
+        name: "Mark Seemann",
+        email: "mark@ploeh.dk",
+        quantity: 4
+      }
+
+      actual = Capacity.check(capacity, get_reserved_seats, reservation)
+
+      expected = {:error, :capacity_exceeded}
       assert expected == actual
     end
   end
--- web/models/capacity.ex
+++ web/models/capacity.ex
@@ -1,6 +1,11 @@
 defmodule Hungry.Capacity do
-  @spec check(non_neg_integer, non_neg_integer, map) :: {:ok, map}
-  def check(_capacity, _reserved_seats, reservation) do
-    {:ok, reservation}
+  @spec check(non_neg_integer, (DateTime.t -> non_neg_integer), map) :: {:ok, map} | {:error, :capacity_exceeded}
+  def check(capacity, get_reserved_seats, reservation) do
+    reserved_seats = get_reserved_seats.(reservation.datetime)
+    if capacity < reservation.quantity + reserved_seats do
+      {:error, :capacity_exceeded}
+    else
+      {:ok, reservation}
+    end
   end
 end
